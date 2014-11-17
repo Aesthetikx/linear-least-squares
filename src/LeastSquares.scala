d6e6ef9b7548495f50aa85e59254bb0e0aee6956
John DeSilva <desilvjo@umich.edu>
2014-11-17 01:18:16 -0500

    Parse data file

--- src/LeastSquares.scala
+++ src/LeastSquares.scala
@@ -1,3 +1,10 @@
 object LeastSquares extends App {
 
+  val filename = "data.txt";
+
+  for (line <- scala.io.Source.fromFile(filename).getLines()) {
+    val Array(step, x, y) = line.split("\\s+");
+    println(s"$step, $x, $y");
+  }
+
 }

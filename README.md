e3f6b7301a6dfea860dcdb2175775e43e872afe1
John DeSilva <desilvjo@umich.edu>
2014-11-25 01:48:30 -0500

    Add error and plot

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ linear-least-squares
 
 Linear Algebra Project using Scala Breeze
 
-![Alt text](/doc/plot.png?raw=true "Quadratic Fit Plot")
+![Alt text](/doc/fit.png?raw=true "Quadratic Fit Plot")
+![Alt text](/doc/error.png?raw=true "Error as a Function of Samples")
--- src/LeastSquares.scala
+++ src/LeastSquares.scala
@@ -2,6 +2,7 @@ import scala.io.Source
 
 import breeze.linalg._
 import breeze.plot._
+import org.jfree.chart.plot.ValueMarker
 
 object LeastSquares extends App {
 
@@ -25,17 +26,52 @@ object LeastSquares extends App {
 
   val xStar: DenseVector[Double] = (inv(A.t * A) * A.t * B).toDenseVector;
 
-  val zStar: DenseVector[Double] = new DenseVector(dataX.map { x =>
+  def zStar(domain: Array[Double]): Array[Double] = domain.map { x =>
     xStar(0) * x * x + xStar(1) * x + xStar(2);
-  });
+  };
 
-  val f = Figure();
-  val p = f.subplot(0);
-  p += plot(dataX, dataZ, '.');
-  p += plot(dataX, zStar);
-  p.title = "Quadratic Approximation of a Tennis Ball Flight";
-  p.xlabel = "X (feet)";
-  p.ylabel = "Z (feet)";
-  f.saveas("plot.png");
+  // Error e of estimation using n samples
+  def e(z: Array[Double], zs: Array[Double], n: Int): Double = {
+    val sum: Double = (z.take(n), zs.take(n)).zipped.map(_-_).map(x => x * x).sum;
+    Math.sqrt(sum) / n;
+  }
+
+  val N: Array[Int] = 1.to(data.size).toArray
+
+  val error: Array[Double] = N.map { n => e(dataZ, zStar(dataX), n) };
+
+  val targetError: Double = 3.0e-3;
+  val minSamples: Int = error.zipWithIndex.find { case (e, i) => e < targetError }.get._2;
+  println("Minimum samples: " + minSamples + ", error: " + error(minSamples));
+
+
+  // Plots
+
+  // Estimation
+  val fitFigure = Figure();
+  val domain: Array[Double] = (0 to 20).toArray.map(_.toDouble)
+  fitFigure.width = 1200;
+  fitFigure.height = 800;
+  val fitPlot = fitFigure.subplot(0);
+  fitPlot += plot(dataX, dataZ, '.');
+  fitPlot += plot(domain, zStar(domain));
+  fitPlot += plot(domain, domain.map { n => 0.0 }, '-');
+  fitPlot.title = "Quadratic Approximation of a Tennis Ball Flight";
+  fitPlot.xlabel = "X (feet)";
+  fitPlot.ylabel = "Z (feet)";
+  fitFigure.saveas("fit.png", 96);
+
+  // Error
+  val errorFigure = Figure();
+  errorFigure.width = 1200;
+  errorFigure.height = 800;
+  val errorPlot = errorFigure.subplot(0);
+  errorPlot += plot(N.map { n => n.toDouble }, error, '-');
+  errorPlot += plot(N.map { n => n.toDouble }, N.map { n => 3.0e-3 }, '-');
+  errorPlot.plot.addDomainMarker(new ValueMarker(minSamples));
+  errorPlot.title = "Error as a function of N";
+  errorPlot.xlabel = "N (samples)";
+  errorPlot.ylabel = "Error (feet)";
+  errorFigure.saveas("error.png", 96);
 
 }
